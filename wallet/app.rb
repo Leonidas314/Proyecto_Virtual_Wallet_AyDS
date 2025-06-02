c10769b2eb37adcf4d6206e82feb35599c448c6b
Lautaro Budin <lautaro_budin@hotmail.com>
2025-06-02 13:29:03 -0300

    Add controller requires in app

--- wallet/app.rb
+++ wallet/app.rb
@@ -1,5 +1,9 @@
-require_relative './config/environment'
 require 'dotenv/load'
+require_relative './config/environment'
+require_relative './controllers/auth_controller'
+require_relative './controllers/dashboard_controller'
+require_relative './controllers/index_controller'
+
 
 enable :sessions
 set :session_secret, ENV['SESSION_SECRET']
