2b5f8860c361820e3040f269713dfc0e02ddd67e
Lautaro Budin <lautaro_budin@hotmail.com>
2025-06-19 17:04:53 -0300

    Fix transfer redirect

--- wallet/app/controllers/transfer_controller.rb
+++ wallet/app/controllers/transfer_controller.rb
@@ -11,32 +11,29 @@ class TransferController < Sinatra::Base
     end
 
   # Procesa la transferencia
-    post '/transfer' do
+  post '/transfer' do
+    user_id = session[:user_id]
+    from_user = User.find_by(id: user_id)
+    cvu = params[:to_cvu]
+    amount = params[:amount].to_f
+    to_user = User.find_by(cvu: cvu)
 
-        user_id = session[:user_id]
-        from_user = User.find_by(id: user_id)
-        puts "DEBUG user_id by session:"
-        puts user_id.inspect
-        puts "DEBUG from_user inspect:"
-        puts from_user.inspect
-        cvu = params[:to_cvu]
-        amount = params[:amount].to_f
-        to_user = User.find_by(cvu: cvu)
-        puts "DEBUG user finded by cbu:"
-        puts to_user.inspect
-        if to_user.nil?
-          @error = "CVU destino no válido."
-        else
-        begin
-          TransferMediator.transferir(from_user, to_user, amount)
-          @success = "Transferencia de $#{amount} realizada con éxito."
-        rescue ArgumentError => e
-          @error = e.message
-        rescue ActiveRecord::RecordInvalid => e
-          @error = "Error de validación: #{e.message}"
-        end
-        end
+    if to_user.nil?
+      @error = "CVU destino no válido."
+      @show_form = false
+    else
+      begin
+        TransferMediator.transferir(from_user, to_user, amount)
+        @success = "Transferencia de $#{amount} realizada con éxito."
+        @show_form = false
+      rescue ArgumentError => e
+        @error = e.message
+        @show_form = false
+      rescue ActiveRecord::RecordInvalid => e
+        @error = "Error de validación: #{e.message}"
+        @show_form = false
+      end
     end
-
-
+    erb :'erb/transfer'
+  end
 end
